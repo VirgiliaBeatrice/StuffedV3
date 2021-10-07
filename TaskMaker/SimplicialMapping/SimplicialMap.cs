c89c916780af4c74bbad6ced36dc955006d5a6dd
Haoyan.Li <haoyan.li.ac@gmail.com>
2021-10-07 20:46:19 +0900

    dev

--- TaskMaker/SimplicialMapping/SimplicialMap.cs
+++ TaskMaker/SimplicialMapping/SimplicialMap.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MathNet.Numerics.LinearAlgebra;
+using SkiaSharp;
+using MathNetExtension;
+using NumSharp;
 
 namespace TaskMaker.SimplicialMapping {
 
@@ -87,6 +90,113 @@ namespace TaskMaker.SimplicialMapping {
             }
 
             return null;
+        }
+    }
+
+    public class SimplexBary {
+        public List<Entity> Basis { get; set; } = new List<Entity>(3);
+        public readonly int Dimension = 3;
+
+        private Matrix<float> A;
+        private Matrix<float> InverseA;
+
+        private void InitializeA() {
+            if (Basis.Count != 3) {
+                Console.WriteLine($"{this}: Basis is not fully set.");
+                return;
+                //throw new Exception($"{this}: Basis is not fully set.");
+            }
+
+            var vBasis = Basis.Select(b => b.ToVector()).ToList();
+            A = Matrix<float>.Build.DenseOfColumnVectors(vBasis);
+            A.InsertRow(0, Vector<float>.Build.Dense(Dimension, 1.0f));
+
+            InverseA = A.Inverse();
+        }
+
+        public void AddBasis(Entity e) {
+            Basis.Add(e);
+            InitializeA();
+        }
+
+        public Vector<float> GetLambdas(SKPoint p) {
+            if (Basis.Count != 3)
+                throw new Exception($"{this}: Basis is not fully set.");
+
+            var vP = p.ToVector().ToColumnMatrix();
+            vP.InsertRow(0, Vector<float>.Build.Dense(1, 1.0f));
+
+            return A.Solve(vP.Column(0));
+        }
+
+        public Vector<float> GetZeroLambdas() {
+            if (Basis.Count != 3)
+                throw new Exception($"{this}: Basis is not fully set.");
+
+            return Vector<float>.Build.Dense(Dimension, 0);
+        }
+    }
+
+    public class ComplexBary<T> {
+        public List<Entity> Basis0 { get; set; } = new List<Entity>();
+        public List<Entity> Basis1 { get; set; } = new List<Entity>();
+        public List<SimplexBary> Barys { get; set; } = new List<SimplexBary>();
+        //public Dictionary<Entity, NDArray> Pairs { get; set; } = new Dictionary<Entity, NDArray>();
+
+        public int Dimension => Basis0.Count;
+
+        private NDArray _tensor;
+        private NDArray _lambdas;
+
+        private void InitializeScalars() {
+            if (Pairs.Values.Any(e => e == null)) {
+                Console.WriteLine($"{this} is not fully config.");
+                return;
+            }
+
+
+        }
+
+        private void InitializeVertices() {
+            Basis0.ForEach(b => Pairs.Add(b, null));
+        }
+
+        public void AddTarget(NDArray target, int[] index) {
+            // Confirm shape be [1, ], a.k.a. 1D
+            target.reshape(new int[] { 1 });
+
+            _tensor.itemset(index, target);
+        }
+
+        
+
+        public Dictionary<Entity, float> GetLambdas(SKPoint p) {
+            // When full set
+            var results = new Dictionary<Entity, float>();
+
+            foreach(var b in Basis) {
+                results.Add(b, 0.0f);
+            }
+
+            foreach(var bary in Barys) {
+                var basis = bary.Basis;
+                var lambdas = bary.GetLambdas(p);
+
+                for(var idx = 0; idx < basis.Count; ++idx) {
+                    results[basis[idx]] += lambdas[idx];
+                }
+            }
+
+            return results;
+        }
+
+        public void Interpolate(SKPoint p) {
+            var lambdas = GetLambdas(p);
+            var list = new List<KeyValuePair<Entity, float>>(lambdas);
+
+
+            _tensor.PointwiseMultiply()
+            lambdas
         }
     }
 
